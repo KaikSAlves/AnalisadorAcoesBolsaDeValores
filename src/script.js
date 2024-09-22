0e224855b4d7650c8c00b6aaa2b43c5b84223309
Kaik Alves <souzaalveskaik@gmail.com>
2024-09-22 14:15:14 -0300

    pequenos ajustes no javaScript

--- src/script.js
+++ src/script.js
@@ -1,29 +1,25 @@
-window.document.getElementById("tabelas").style.display = "flex";
+window.document.getElementById("main").style.display = "flex";
 
 function mudarPaginaTabelas() {
     window.document.getElementById("tabelas").style.display = "flex";
     window.document.getElementById("graficos").style.display = "none";
-    window.document.getElementById("tendencias").style.display = "none";
     window.document.getElementById("main").style.display = "none";
 }
 
 function mudarPaginaGraficos() {
     window.document.getElementById("tabelas").style.display = "none";
     window.document.getElementById("graficos").style.display = "flex";
-    window.document.getElementById("tendencias").style.display = "none";
     window.document.getElementById("main").style.display = "none";
 }
 
 function mudarPaginaTendencias() {
     window.document.getElementById("tabelas").style.display = "none";
     window.document.getElementById("graficos").style.display = "none";
-    window.document.getElementById("tendencias").style.display = "flex";
     window.document.getElementById("main").style.display = "none";
 }
 
 function mudarPaginaMain() {
     window.document.getElementById("tabelas").style.display = "none";
     window.document.getElementById("graficos").style.display = "none";
-    window.document.getElementById("tendencias").style.display = "none";
     window.document.getElementById("main").style.display = "flex";
 }
